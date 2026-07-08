994013f8c9663be901ee33d00815fd8e991ebbcc
jappa <909513118@qq.com>
2026-07-08 10:00:25 +0800

    更新标题

--- backend/open_webui/main.py
+++ backend/open_webui/main.py
@@ -427,7 +427,7 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(
-    title='Open WebUI',
+    title='Aussco AI',
     docs_url='/docs' if ENV == 'dev' else None,
     openapi_url='/openapi.json' if ENV == 'dev' else None,
     redoc_url=None,
